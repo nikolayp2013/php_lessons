4c07c5aa149eea11bee539f91c78a2cd8d56e208
Nikolay Perov <nikolayp2013@gmail.com>
2019-06-14 09:53:14 +0400

    initial project version

---
+++ lesson_2.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+  <meta charset="UTF-8">
+  <title>PHP</title>
+</head>
+<body>
+  <?php
+    // echo '<b>Привет</b><br>' . 'Привет2' . "<br>Hi"; Комментарий на одну строку
+    # echo '<b>Привет</b><br>' . 'Привет2' . "<br>Hi"; Комментарий на одну строку
+    /*
+      Большой комментарий
+    */
+    echo '<b>Привет</b><br>' . 'Привет2' . "<br>Hi";
+    echo '\'<br>';
+    echo "\"<br>";
+    echo "\\<br>";
+    //echo '<b>Привет</b><br>' . 'Привет2' . "<br>Hi <input type='text'>";
+  ?>
+</body>
+</html>
